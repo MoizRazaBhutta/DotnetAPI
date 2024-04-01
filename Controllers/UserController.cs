eafcf1db46e3774b53152c3539802bdc9dc6d4d6
MoizRazaBhutta <moiz_bhutta@hotmail.com>
2024-04-01 16:17:43 -0400

    Database Connection

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -6,8 +6,19 @@ namespace DotnetAPI.Controllers;
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
-    public UserController()
+    // Create dapper private var
+    DataContextDapper _dapper;
+    public UserController(IConfiguration configuration)
     {
+        // Access Dapper and pass the config to access the connection string into that class
+        _dapper = new DataContextDapper(configuration);
+    }
+
+    [HttpGet("TestConnection")]
+    public DateTime TestConnection()
+    {
+        // Run dapper with SQL Query to return the result from the database
+        return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
     }
 
     [HttpGet("GetUsers/{testValue}")]
